84388da0879071ff354782df69b01358ca5ceed7
maul.esel <maul.esel@go4more.de>
2012-08-27 12:28:23 +0200

    rename registration URLs config to plural

--- config/registration.php
+++ config/registration.php
@@ -5,7 +5,7 @@
 	# (http://dev.mysql.com/doc/refman/5.5/en/date-and-time-functions.html#function_date-add).
 
 	# a list of URLs to POST data to after a successful registration, separated by space
-	define('POST_REGISTRATION_URL', '');
+	define('POST_REGISTRATION_URLS', '');
 	# each of these URLs receives the following POST data:
 	#   + user name ('user')
 	#   + mail address ('mail')
