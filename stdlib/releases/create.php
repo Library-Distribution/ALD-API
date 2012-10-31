ab449494ae82365fbc0a2479a9182ad6577a8f9c
maul.esel <maul.esel@go4more.de>
2012-10-31 21:51:47 +0100

    begin implementing stdlib release creation

---
+++ stdlib/releases/create.php
@@ -0,0 +1,158 @@
+<?php
+	require_once("../../util.php");
+	require_once("../../Assert.php");
+	require_once("../../HttpException.php");
+	require_once("../../UpdateType.php");
+	require_once("../../semver.php");
+
+	define('UPDATE_TYPE_PATCH', 2);
+	define('UPDATE_TYPE_MINOR', 3);
+	define('UPDATE_TYPE_MAJOR', 4);
+
+	try
+	{
+		#Assert::RequestMethod("POST");
+		Assert::GetParameters("type");
+		$content_type = get_preferred_mimetype(array("application/json", "text/xml", "application/xml"), "application/json");
+		$type = UpdateType::getCode($_GET["type"], "stdlib_releases");
+
+		$db_connection = db_ensure_connection();
+
+		# get latest release # TODO: fix for non-published
+		$db_query = "SELECT `release` FROM " . DB_TABLE_STDLIB_RELEASES . " ORDER BY date DESC LIMIT 1";
+		$db_result = mysql_query($db_query, $db_connection);
+		if (!$db_result)
+		{
+			throw new HttpException(500, NULL, mysql_error());
+		}
+		if (mysql_num_rows($db_result) != 1)
+		{
+			$prev_release = "0.0.0";
+		}
+		else
+		{
+			$db_entry = mysql_fetch_assoc($db_result);
+			$prev_release = $db_entry["release"];
+		}
+
+		# todo: bump version number according to $type
+		$release = array();
+		semver_parts($prev_release, $release);
+
+		/*
+		if ($type == UPDATE_TYPE_PATCH || $type == UPDATE_TYPE_MINOR || $type == UPDATE_TYPE_MAJOR)
+		{
+			unset($release["prerelease"]);
+			unset($release["build"]);
+			$release["patch"]++;
+
+			if ($type == UPDATE_TYPE_MINOR || $type == UPDATE_TYPE_MAJOR)
+			{
+				$release["patch"] = 0;
+				$release["minor"]++;
+
+				if ($type == UPDATE_TYPE_MAJOR)
+				{
+					$release["minor"] = 0;
+					$release["major"]++;
+				}
+			}
+		}
+		*/
+		while (true)
+		{
+			semver_bump_version($release, $type);
+			$db_query = "SELECT * FROM " . DB_TABLE_STDLIB_RELEASES . " WHERE `release` = '" . semver_string($release) . "'";
+			$db_result = mysql_query($db_query, $db_connection);
+			if (!$db_result)
+			{
+				throw new HttpException(500, NULL, mysql_error());
+			}
+			if (mysql_num_rows($db_result) != 1)
+			{
+				break;
+			}
+		}
+		$release = semver_string($release);
+
+		$data = array("release" => $release, "update" => $type);
+		if (isset($_POST["version"]))
+		{
+			try {
+				$result = semver_compare($_POST["version"], $release);
+			} catch (Exception $e) {
+				throw new HttpException(400, NULL, "Bad release version!"); # semver could not validate
+			}
+			if ($result != 1)
+				throw new HttpException(400, NULL, "Bad release version!"); # version is smaller then minimum
+
+			$data["release"] = mysql_real_escape_string($_POST["version"], $db_connection);
+		}
+		if (isset($_POST["date"]))
+		{
+			$data["date"] = mysql_real_escape_string($_POST["description"], $db_connection);
+		}
+		if (isset($_POST["description"]))
+		{
+			$data["description"] = mysql_real_escape_string($_POST["description"], $db_connection);
+		}
+
+		$db_query = "INSERT INTO " . DB_TABLE_STDLIB_RELEASES
+				. " ("
+				. implode(", ", array_map(function($item) { return "`$item`"; }, array_keys($data)))
+				. ") VALUES ("
+				. implode(", ", array_map(function($item) { return "'$item'"; }, array_values($data)))
+				. ")";
+		$db_result = mysql_query($db_query, $db_connection);
+		if (!$db_result)
+		{
+			throw new HttpException(500, NULL, mysql_error());
+		}
+		if (mysql_affected_rows() != 1)
+		{
+			throw new HttpException(500);
+		}
+
+		if ($content_type == "application/json")
+		{
+			$content = json_encode(array("release" => $data["release"]));
+		}
+		else if ($content_type == "text/xml" || $content_type == "application/xml")
+		{
+			$content = "<ald:version xmlns:ald=\"ald://api/stdlib/releases/create/schema/2012\">$data[release]</ald:version>";
+		}
+		header("HTTP/1.1 200 " . HttpException::getStatusMessage(200));
+		header("Content-type: $content_type");
+		echo $content;
+	}
+	catch (HttpException $e)
+	{
+		handleHttpException($e);
+	}
+	catch (Exception $e)
+	{
+		handleHttpException(new HttpException(500, NULL, $e->getMessage()));
+	}
+
+	function semver_bump_version(array &$release, $type)
+	{
+		if ($type == UPDATE_TYPE_PATCH || $type == UPDATE_TYPE_MINOR || $type == UPDATE_TYPE_MAJOR)
+		{
+			unset($release["prerelease"]);
+			unset($release["build"]);
+			$release["patch"]++;
+
+			if ($type == UPDATE_TYPE_MINOR || $type == UPDATE_TYPE_MAJOR)
+			{
+				$release["patch"] = 0;
+				$release["minor"]++;
+
+				if ($type == UPDATE_TYPE_MAJOR)
+				{
+					$release["minor"] = 0;
+					$release["major"]++;
+				}
+			}
+		}
+	}
+?>
