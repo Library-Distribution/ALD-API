e9ba7f6e166d76c51df1f1fa11b8c938e3083b47
maul.esel <maul.esel@go4more.de>
2012-11-18 18:33:11 +0100

    item description: simplify code with Item::getId() method

--- items/describe.php
+++ items/describe.php
@@ -5,6 +5,7 @@
 	require_once("../User.php");
 	require_once("../Assert.php");
 	require_once("../semver.php");
+	require_once('../Item.php');
 
 	require_once("../config/upload.php"); # import upload settings, including upload folder!
 
@@ -22,44 +23,7 @@
 
 		if (!isset($_GET["id"]))
 		{
-			$version = mysql_real_escape_string($_GET["version"], $db_connection);
-
-			$db_version_cond = " AND version = '$version'";
-			if (in_array($version, array("latest", "first")))
-			{
-				$special_version = $version;
-				$db_version_cond = ""; # read all and filter below
-			}
-
-			$db_query = "SELECT HEX(id), version FROM " . DB_TABLE_ITEMS . " WHERE name = '" . mysql_real_escape_string($_GET["name"], $db_connection) . "' $db_version_cond AND reviewed != '-1'";
-			$db_result = mysql_query($db_query, $db_connection);
-
-			if (!$db_result)
-			{
-				throw new HttpException(500);
-			}
-			if (mysql_num_rows($db_result) < 1)
-			{
-				throw new HttpException(404);
-			}
-
-			if (isset($special_version)) # filter now if special version was used
-			{
-				$items = array(); # fetch all items in an array
-				while ($row = mysql_fetch_assoc($db_result))
-				{
-					$items[] = $row;
-				}
-
-				usort($items, "semver_sort"); # sort by "version" field, following semver rules
-				$db_entry = $items[$special_version == "latest" ? count($items) - 1 : 0];
-			}
-			else
-			{
-				$db_entry = mysql_fetch_assoc($db_result);
-			}
-
-			$id = $db_entry["HEX(id)"];
+			$id = Item::getId($_GET['name'], $_GET['version']);
 		}
 		else
 		{
@@ -217,10 +181,5 @@
 			$content .= "</ald:version-list>";
 		}
 		return $content;
-	}
-
-	function semver_sort($a, $b)
-	{
-		return semver_compare($a["version"], $b["version"]);
 	}
 ?>
