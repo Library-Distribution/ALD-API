3e2010f785771983b136a65d532b4b4504592e61
maul.esel <maul.esel@go4more.de>
2012-11-22 15:16:55 +0100

    fix StdlibPending class

--- stdlib/StdlibPending.php
+++ stdlib/StdlibPending.php
@@ -1,14 +1,12 @@
 <?php
-require_once('../../db.php');
-require_once('../../HttpException.php');
+require_once(__DIR__ . '/../db.php');
+require_once(__DIR__ . '/../HttpException.php');
 
 class StdlibPending
 {
 	public static function GetEntries()
 	{
 		$db_connection = db_ensure_connection();
-		$release = mysql_real_escape_string($release, $db_connection);
-
 		$db_query = 'SELECT HEX(`lib`) FROM ' . DB_TABLE_STDLIB_PENDING;
 		$db_result = mysql_query($db_query, $db_connection);
 		if (!$db_result)
