91a082a2c9156f7c49ffa3cc88e7bee57220e2a4
maul.esel <maul.esel@go4more.de>
2013-02-09 17:12:21 +0100

    shorten rating code with Item class
    The frequently used code for retrieving an ID for name and
    version has on master been moved to the Item class. Use it
    now that it's finally available for the rating page.

--- items/rating.php
+++ items/rating.php
@@ -1,5 +1,6 @@
 <?php
 	require_once("../User.php");
+	require_once("../Item.php");
 	require_once("../Assert.php");
 	require_once("../util.php");
 	require_once("../db.php");
@@ -17,44 +18,7 @@
 
 		if (!isset($_GET["id"]))
 		{
-			$version = mysql_real_escape_string($_GET["version"], $db_connection);
-
-			$db_version_cond = " AND version = '$version'";
-			if (in_array($version, array("latest", "first")))
-			{
-				$special_version = $version;
-				$db_version_cond = ""; # read all and filter below
-			}
-
-			$db_query = "SELECT HEX(id), version FROM " . DB_TABLE_ITEMS . " WHERE name = '" . mysql_real_escape_string($_GET["name"], $db_connection) . "' $db_version_cond AND reviewed != '-1'";
-			$db_result = mysql_query($db_query, $db_connection);
-
-			if (!$db_result)
-			{
-				throw new HttpException(500);
-			}
-			if (mysql_num_rows($db_result) < 1)
-			{
-				throw new HttpException(404);
-			}
-
-			if (isset($special_version)) # filter now if special version was used
-			{
-				$items = array(); # fetch all items in an array
-				while ($row = mysql_fetch_assoc($db_result))
-				{
-					$items[] = $row;
-				}
-
-				usort($items, "semver_sort"); # sort by "version" field, following semver rules
-				$db_entry = $items[$special_version == "latest" ? count($items) - 1 : 0];
-			}
-			else
-			{
-				$db_entry = mysql_fetch_assoc($db_result);
-			}
-
-			$id = $db_entry["HEX(id)"];
+			$id = Item::getId($_GET['name'], $_GET['version']);
 		}
 		else
 		{
