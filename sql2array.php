147c4101e226a64897801db6bf6f85dfb0ba3a48
maul.esel <maul.esel@go4more.de>
2013-02-13 20:02:16 +0100

    change sql2array() to allow different kinds of callbacks

--- sql2array.php
+++ sql2array.php
@@ -4,7 +4,7 @@ function sql2array($db_result, $item_handler = NULL) {
 	$i = 0;
 	while ($db_row = mysql_fetch_assoc($db_result)) {
 		$key = $i;
-		$arr[$key] = $item_handler !== NULL ? $item_handler($db_row, $key) : $db_row;
+		$arr[$key] = $item_handler !== NULL ? (is_array($item_handler) ? call_user_func($item_handler, $db_row, $key) : $item_handler($db_row, $key)) : $db_row;
 		$i++;
 	}
 	return $arr;
