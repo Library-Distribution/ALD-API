8730de6c2db6cd42876d4b00b6792c82f08c29e1
maul.esel <maul.esel@go4more.de>
2013-02-13 20:26:55 +0100

    When validating login, check for suspensions instead of old token

--- User.php
+++ User.php
@@ -1,5 +1,6 @@
 <?php
 require_once("db.php");
+require_once(dirname(__FILE__) . '/users/Suspension.php');
 require_once("modules/HttpException/HttpException.php");
 
 class User
@@ -83,7 +84,7 @@ class User
 		$pw = hash("sha256", $pw);
 		$escaped_user = mysql_real_escape_string($user, $db_connection);
 
-		$db_query = "SELECT pw, activationToken FROM " . DB_TABLE_USERS . " WHERE name = '$escaped_user'";
+		$db_query = "SELECT pw FROM " . DB_TABLE_USERS . " WHERE name = '$escaped_user'";
 		$db_result = mysql_query($db_query, $db_connection);
 		if (!$db_result)
 		{
@@ -104,14 +105,6 @@ class User
 		}
 
 		$data = mysql_fetch_object($db_result);
-		if ($data->activationToken)
-		{
-			if (!$throw)
-			{
-				return false;
-			}
-			throw new HttpException(403, NULL, "Account is currently deactivated.");
-		}
 		if ($data->pw != $pw)
 		{
 			if (!$throw)
@@ -119,6 +112,13 @@ class User
 				return false;
 			}
 			throw new HttpException(403, NULL, "Invalid credentials were specified.");
+		}
+
+		if (Suspension::isSuspended($user)) { # check here (not above) to make sure others can't see the suspension
+			if (!$throw) {
+				return false;
+			}
+			throw new HttpException(403, NULL, 'Account is currently suspended.');
 		}
 		return true;
 	}
