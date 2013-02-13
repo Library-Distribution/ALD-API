124391fa24a085ffbd188e9b4b2299130ccea193
maul.esel <maul.esel@go4more.de>
2013-02-13 18:29:58 +0100

    add database table for suspensions

--- config/database.php
+++ config/database.php
@@ -10,7 +10,9 @@
 	define('DB_TABLE_USERS', 'users'); # 
 	define('DB_TABLE_TYPES', 'types'); # 
 
-	define('DB_TABLE_RATINGS', 'ratings'); #
+	define('DB_TABLE_RATINGS', 'ratings'); # 
+
+	define('DB_TABLE_SUSPENSIONS', 'suspensions'); # 
 
 	# the credentials for accessing the database
 	define('DB_USERNAME', 'root'); # the user name
