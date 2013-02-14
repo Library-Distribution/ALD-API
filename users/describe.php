ed23b2aaff411197b1c3f13929d94363b015686e
maul.esel <maul.esel@go4more.de>
2013-02-14 16:35:27 +0100

    user description code cleanup
    Use the existing credential code instead of duplicating it.
    Fix a mistake with mail hashing. Rename a variable du to up-
    coming changes.

--- users/describe.php
+++ users/describe.php
@@ -26,7 +26,7 @@
 			$id = mysql_real_escape_string($_GET["id"], $db_connection);
 		}
 
-		$db_query = "SELECT name, mail, pw, privileges, joined, activationToken FROM " . DB_TABLE_USERS . " WHERE id = UNHEX('$id')";
+		$db_query = "SELECT name, mail, privileges, joined, activationToken FROM " . DB_TABLE_USERS . " WHERE id = UNHEX('$id')";
 		$db_result = mysql_query($db_query, $db_connection);
 		if (!$db_result)
 		{
@@ -36,28 +36,20 @@
 		if (mysql_num_rows($db_result) == 1)
 		{
 			$user = mysql_fetch_assoc($db_result);
-			$include_mail = false;
+			$trusted_user = false;
 
-			if (isset($_SERVER["PHP_AUTH_USER"]) && isset($_SERVER["PHP_AUTH_PW"]))
-			{
-				Assert::HTTPS();
-				$password_hash = hash("sha256", $_SERVER["PHP_AUTH_PW"]);
-
-				if ($_SERVER["PHP_AUTH_USER"] == $user["name"] && $password_hash == $user["pw"]) # user requests information about himself - OK.
-				{
-					$include_mail = true;
-				}
-				else
-				{
-					User::validateLogin($_SERVER["PHP_AUTH_USER"], $_SERVER["PHP_AUTH_PW"]); # check if correct credentials specified
-					$encrypt_mail =  !User::hasPrivilege($_SERVER["PHP_AUTH_USER"], User::PRIVILEGE_USER_MANAGE) && !User::hasPrivilege($_SERVER["PHP_AUTH_USER"], User::PRIVILEGE_ADMIN); # admin and user moderators may request this, too
-				}
+			if (isset($_SERVER["PHP_AUTH_USER"]) && isset($_SERVER["PHP_AUTH_PW"])) {
+				user_basic_auth(''); # if credentials are specified, they must be correct
+				$trusted_user = $_SERVER['PHP_AUTH_USER'] == $user['name'] # user requests information about himself - OK.
+							|| User::hasPrivilege($_SERVER['PHP_AUTH_USER'], User::PRIVILEGE_USER_MANAGE) # admins and moderators can see the mail address, to
+							|| User::hasPrivilege($_SERVER['PHP_AUTH_USER'], User::PRIVILEGE_ADMIN);
 			}
+
 			$user["mail-md5"] = md5($user["mail"]);
 			$user["id"] = $id;
 			$user["enabled"] = !$user["activationToken"]; unset($user["activationToken"]);
 
-			if (!$include_mail)
+			if (!$trusted_user)
 			{
 				unset($user["mail"]);
 			}
