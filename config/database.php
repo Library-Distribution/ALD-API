148598e56b7afb594c026b127e9fe2df4ace2246
maul.esel <maul.esel@go4more.de>
2013-02-28 18:05:18 +0100

    add config for new DBs

--- config/database.php
+++ config/database.php
@@ -12,6 +12,10 @@
 	define('DB_TABLE_STDLIB_RELEASES', 'stdlib_releases'); # 
 	define('DB_TABLE_STDLIB_PENDING', 'stdlib_pending'); # 
 	define('DB_TABLE_STDLIB_ACTIONS', 'stdlib_actions'); # 
+
+	define('DB_TABLE_CANDIDATES', 'candidates'); # 
+	define('DB_TABLE_CANDIDATE_RATING', 'candidate_rating'); # 
+
 	define('DB_TABLE_REGISTRATION', 'registration'); # 
 	define('DB_TABLE_TYPES', 'types'); # 
 
