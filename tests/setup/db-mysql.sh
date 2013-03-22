467ae9d6200f1141f8c3f1957e085a2af6030efb
maul.esel <maul.esel@go4more.de>
2013-03-22 17:33:29 +0100

    Adjust mysql encoding to travis

--- tests/setup/db-mysql.sh
+++ tests/setup/db-mysql.sh
@@ -4,14 +4,14 @@ echo "Setting up MySQL for the tests..."
 
 # create the database
 echo "    Creating the database..."
-mysql --default-character-set=latin1 -u root -e 'CREATE DATABASE IF NOT EXISTS `travis-test`;'
+mysql --default-character-set=utf8 -u root -e 'CREATE DATABASE IF NOT EXISTS `travis-test`;'
 
 # import the individual tables
 echo "    Importing the DB tables..."
 for file in MySQL/*.sql;
 do
 	echo "        $file"
-	mysql --default-character-set=latin1 -u root travis-test < "$file"
+	mysql --default-character-set=utf8 -u root travis-test < "$file"
 done
 
 php -r '$conn = mysql_connect("localhost", "root", ""); echo "    MySQL client encoding: ", mysql_client_encoding($conn), "\n\n";'
