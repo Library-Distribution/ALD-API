a3e2c6c92be4983f4cb4b3127c8c53dcafe3bc03
maul.esel <maul.esel@go4more.de>
2013-03-22 17:32:55 +0100

    improve output for MySQL setup [ci skip]

--- tests/setup/db-mysql.sh
+++ tests/setup/db-mysql.sh
@@ -1,12 +1,17 @@
 #! /usr/bin/env sh
 
+echo "Setting up MySQL for the tests..."
+
 # create the database
+echo "    Creating the database..."
 mysql --default-character-set=latin1 -u root -e 'CREATE DATABASE IF NOT EXISTS `travis-test`;'
 
 # import the individual tables
+echo "    Importing the DB tables..."
 for file in MySQL/*.sql;
 do
+	echo "        $file"
 	mysql --default-character-set=latin1 -u root travis-test < "$file"
 done
 
-php -r '$conn = mysql_connect("localhost", "root", ""); echo "MySQL client encoding: ", mysql_client_encoding($conn), "\n\n";'
+php -r '$conn = mysql_connect("localhost", "root", ""); echo "    MySQL client encoding: ", mysql_client_encoding($conn), "\n\n";'
