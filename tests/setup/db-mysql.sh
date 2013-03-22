1d28988879fc3efaa2abf91fd2a11f9d7dc9d4c4
maul.esel <maul.esel@go4more.de>
2013-03-22 16:56:31 +0100

    testing: try changing character set to make tests pass

--- tests/setup/db-mysql.sh
+++ tests/setup/db-mysql.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env sh
 
 # create the database
-mysql -u root -e 'CREATE DATABASE IF NOT EXISTS `travis-test`;'
+mysql --default-character-set=latin1 -u root -e 'CREATE DATABASE IF NOT EXISTS `travis-test`;'
 
 # import the individual tables
 for file in MySQL/*.sql;
 do
-	mysql -u root travis-test < "$file"
+	mysql --default-character-set=latin1 -u root travis-test < "$file"
 done
