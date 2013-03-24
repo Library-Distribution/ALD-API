4a5fec911a19ff882c167bb80376252176d86ca3
maul.esel <maul.esel@go4more.de>
2013-03-24 16:02:45 +0100

    support prerelease and build parts when comparing semver

--- MySQL/code/semver_compare.sql
+++ MySQL/code/semver_compare.sql
@@ -26,12 +26,75 @@ CREATE FUNCTION semver_compare(version1 varchar(50), version2 varchar(50))
 		END IF;
 
 		IF (@pre1 IS NULL AND @pre2 IS NOT NULL) THEN
-			RETURN 1;
+			RETURN +1;
 		ELSEIF (@pre1 IS NOT NULL AND @pre2 IS NULL) THEN
 			RETURN -1;
+		ELSEIF (@pre1 IS NOT NULL AND @pre2 IS NOT NULL) THEN
+			-- compare each prerelease part
+			SELECT (LENGTH(@pre1) - LENGTH(REPLACE(@pre1, '.', ''))) / LENGTH('.') + 1 INTO @pre_parts1; -- count the number of parts
+			SELECT (LENGTH(@pre2) - LENGTH(REPLACE(@pre2, '.', ''))) / LENGTH('.') + 1 INTO @pre_parts2;
+			SET @m = 1;
+
+			WHILE (@m <= LEAST(@pre_parts1, @pre_parts2)) DO
+				SET @part1 = TRIM(LEADING CONCAT(SUBSTRING_INDEX(@pre1, '.', @m - 1), '.') FROM SUBSTRING_INDEX(@pre1, '.', @m));
+				SET @part2 = TRIM(LEADING CONCAT(SUBSTRING_INDEX(@pre2, '.', @m - 1), '.') FROM SUBSTRING_INDEX(@pre2, '.', @m));
+
+				IF (@part1 RLIKE '^[[:digit:]]+$' AND @part2 RLIKE '^[[:digit:]]+$') THEN
+					SET @part1 = CAST(@part1 AS SIGNED), @part2 = CAST(@part2 AS SIGNED);
+					IF (@part1 < @part2) THEN
+						RETURN -1;
+					ELSEIF (@part1 > @part2) THEN
+						RETURN +1;
+					END IF;
+				ELSEIF ((@cmp := STRCMP(@part1, @part2)) != 0) THEN
+					RETURN @cmp;
+				END IF;
+
+				SET @m = @m + 1;
+			END WHILE;
+
+			IF (@pre_parts1 < @pre_parts2) THEN -- the longer one wins
+				RETURN -1;
+			ELSEIF (@pre_parts2 > @pre_parts1) THEN
+				RETURN +1;
+			END IF;
 		END IF;
 
-		-- TODO: no full prerelease / build support so far
+		IF (@build1 IS NULL AND @build2 IS NOT NULL) THEN
+			RETURN -1;
+		ELSEIF (@build1 IS NOT NULL AND @build2 IS NULL) THEN
+			RETURN 1;
+		ELSEIF (@build1 IS NOT NULL AND @build2 IS NOT NULL) THEN
+			-- compare each build part
+			SELECT (LENGTH(@build1) - LENGTH(REPLACE(@build1, '.', ''))) / LENGTH('.') + 1 INTO @build_parts1; -- count the number of parts
+			SELECT (LENGTH(@build2) - LENGTH(REPLACE(@build2, '.', ''))) / LENGTH('.') + 1 INTO @build_parts2;
+			SET @m = 1;
+
+			WHILE (@m <= LEAST(@build_parts1, @build_parts2)) DO
+				SET @part1 = TRIM(LEADING CONCAT(SUBSTRING_INDEX(@build1, '.', @m - 1), '.') FROM SUBSTRING_INDEX(@build1, '.', @m));
+				SET @part2 = TRIM(LEADING CONCAT(SUBSTRING_INDEX(@build2, '.', @m - 1), '.') FROM SUBSTRING_INDEX(@build2, '.', @m));
+
+				IF (@part1 RLIKE '^[[:digit:]]+$' AND @part2 RLIKE '^[[:digit:]]+$') THEN
+					SET @part1 = CAST(@part1 AS SIGNED), @part2 = CAST(@part2 AS SIGNED);
+					IF (@part1 < @part2) THEN
+						RETURN -1;
+					ELSEIF (@part1 > @part2) THEN
+						RETURN +1;
+					END IF;
+				ELSEIF ((@cmp := STRCMP(@part1, @part2)) != 0) THEN
+					RETURN @cmp;
+				END IF;
+
+				SET @m = @m + 1;
+			END WHILE;
+
+			IF (@build_parts1 < @build_parts2) THEN -- the longer one wins
+				RETURN -1;
+			ELSEIF (@build_parts1 > @build_parts2) THEN
+				RETURN +1;
+			END IF;
+		END IF;
+
 		RETURN 0;
 	END
 	//
