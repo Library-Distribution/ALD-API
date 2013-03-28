b8d60b73366386b63c18412589f5dc9be10b4f50
maul.esel <maul.esel@go4more.de>
2013-03-28 21:58:47 +0100

    remove useless output of client encoding

--- tests/setup/db-mysql.sh
+++ tests/setup/db-mysql.sh
@@ -13,5 +13,3 @@ do
 	echo "        $file"
 	mysql --default-character-set=utf8 -u root travis-test < "$file"
 done
-
-php -r '$conn = mysql_connect("localhost", "root", ""); echo "    MySQL client encoding: ", mysql_client_encoding($conn), " (will be changed to latin1)\n\n";'
