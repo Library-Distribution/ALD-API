22cbe4578525d88fc99821f05f60f3251e36dd0e
maul.esel <maul.esel@go4more.de>
2013-04-06 14:16:13 +0200

    new: support YAML output by adding a working YamlConverter

---
+++ YamlConverter.php
@@ -0,0 +1,13 @@
+<?php
+require_once(dirname(__FILE__) . '/OutputConverter.php');
+
+class YamlConverter implements OutputConverter {
+	public function canRun() {
+		return function_exists('yaml_emit');
+	}
+
+	public function convert(array $data, $nodes = NULL) {
+		return yaml_emit($data);
+	}
+}
+?>
