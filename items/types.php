48433a2cb27d5cee019fc6494eee59df82d6d438
maul.esel <maul.esel@go4more.de>
2013-04-06 20:00:14 +0200

    add missing error handling for items/types

--- items/types.php
+++ items/types.php
@@ -3,25 +3,32 @@
 	require_once('../Assert.php');
 	require_once('ItemType.php');
 
-	Assert::RequestMethod(Assert::REQUEST_METHOD_GET);
-	$content_type = get_preferred_mimetype(array('application/json', 'text/xml', 'application/xml'), 'application/json');
+	try {
+		Assert::RequestMethod(Assert::REQUEST_METHOD_GET);
+		$content_type = get_preferred_mimetype(array('application/json', 'text/xml', 'application/xml'), 'application/json');
 
-	$types = ItemType::getAllNames();
+		$types = ItemType::getAllNames();
 
-	if ($content_type == 'application/json')
-	{
-		$content = json_encode($types);
+		if ($content_type == 'application/json')
+		{
+			$content = json_encode($types);
+		}
+		else
+		{
+			$content = '<?xml version="1.0" encoding="utf-8" ?><ald:types xmlns:ald="ald://api/items/types/schema/2012">' ;
+			foreach ($types AS $type)
+				$content .= '<ald:type ald:name="' .  htmlspecialchars($type, ENT_QUOTES) . '"/>';
+			$content .= '</ald:types>';
+		}
+
+		header('HTTP/1.1 200 ' . HttpException::getStatusMessage(200));
+		header('Content-type: ' . $content_type);
+		echo $content;
+		exit;
+
+	} catch (HttpException $e) {
+		handleHttpException($e);
+	} catch (Exception $e) {
+		handleHttpException(new HttpException(500, NULL, $e->getMessage()));
 	}
-	else
-	{
-		$content = '<?xml version="1.0" encoding="utf-8" ?><ald:types xmlns:ald="ald://api/items/types/schema/2012">' ;
-		foreach ($types AS $type)
-			$content .= '<ald:type ald:name="' .  htmlspecialchars($type, ENT_QUOTES) . '"/>';
-		$content .= '</ald:types>';
-	}
-
-	header('HTTP/1.1 200 ' . HttpException::getStatusMessage(200));
-	header('Content-type: ' . $content_type);
-	echo $content;
-	exit;
 ?>
