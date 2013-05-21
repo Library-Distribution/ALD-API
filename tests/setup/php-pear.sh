a2d59dd9a2c757065aa299e32117927b99b94e01
maul.esel <maul.esel@go4more.de>
2013-05-22 01:05:01 +0200

    fix installation script to omit http:// prefix

--- tests/setup/php-pear.sh
+++ tests/setup/php-pear.sh
@@ -2,10 +2,10 @@
 echo "Installing PHP packages..."
 if [ "$TRAVIS_PHP_VERSION" = "5.5" ]; then
 	echo "	Using pyrus..."
-	pyrus channel-discover http://pear.phpunit.de
+	pyrus channel-discover pear.phpunit.de
 	pyrus install phpunit/DbUnit
 else
 	echo "	Using pear..."
-	pear channel-discover http://pear.phpunit.de
+	pear channel-discover pear.phpunit.de
 	pear install phpunit/DbUnit
 fi
