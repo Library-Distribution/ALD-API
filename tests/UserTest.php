41f639b2b8c0c24944ab320ac8787963f5f05a93
maul.esel <maul.esel@go4more.de>
2013-05-21 13:40:48 +0200

    fix phpunit tests

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,15 +1,14 @@
 <?php
-require_once('PHPUnit/Autoload.php');
-require_once('PHPUnit/Framework/Assert/Functions.php'); # should not be required (?)
+require_once('PHPUnit/Framework/TestCase.php');
 
 require_once(dirname(__FILE__) . '/../User.php');
 require_once(dirname(__FILE__) . '/../db.php');
 
 class UserTest extends PHPUnit_Framework_TestCase {
-	public static function testCreate() {
+	public function testCreate() {
 		User::create('Bob', 'bob@example.com', 'secret1234');
-		assertTrue(User::existsName('Bob'), 'Failed to create user "Bob" (no user with this name found)');
-		assertTrue(User::existsMail('bob@example.com'), 'Failed to create user "Bob" (no user with this mail address found)');
+		$this->assertTrue(User::existsName('Bob'), 'Failed to create user "Bob" (no user with this name found)');
+		$this->assertTrue(User::existsMail('bob@example.com'), 'Failed to create user "Bob" (no user with this mail address found)');
 	}
 
 	/**
@@ -17,7 +16,7 @@ class UserTest extends PHPUnit_Framework_TestCase {
 	* @expectedException HttpException
 	* @expectedExceptionCode 500
 	*/
-	public static function testCreateDuplicateName() {
+	public function testCreateDuplicateName() {
 		User::create('Bob', 'bob2@example.com', 'some-pw');
 	}
 
@@ -26,44 +25,44 @@ class UserTest extends PHPUnit_Framework_TestCase {
 	* @expectedException HttpException
 	* @expectedExceptionCode 500
 	*/
-	public static function testCreateDuplicateMail() {
+	public function testCreateDuplicateMail() {
 		User::create('Paul', 'bob@example.com', 'some-pw');
 	}
 
 	/**
 	* @depends testCreate
 	*/
-	public static function testID() {
+	public function testID() {
 		$id = User::getID('Bob');
-		assertRegExp('/[0-9a-fA-F]{32}/', $id, 'Invalid user ID for "Bob": "' . $id . '"');
+		$this->assertRegExp('/[0-9a-fA-F]{32}/', $id, 'Invalid user ID for "Bob": "' . $id . '"');
 	}
 
 	/**
 	* @depends testID
 	*/
-	public static function testName() {
+	public function testName() {
 		$id = User::getID('Bob');
-		assertEquals(User::getName($id), 'Bob', 'Could not retrieve the user name for a given ID of "' . $id . '"');
+		$this->assertEquals(User::getName($id), 'Bob', 'Could not retrieve the user name for a given ID of "' . $id . '"');
 	}
 
 	/**
 	* @depends testCreate
 	*/
-	public static function testLogin() {
-		assertTrue(User::validateLogin('Bob', 'secret1234'), 'Could not login as "Bob" with password "secret1234"');
+	public function testLogin() {
+		$this->assertTrue(User::validateLogin('Bob', 'secret1234'), 'Could not login as "Bob" with password "secret1234"');
 	}
 
 	/**
 	* @depends testID
 	*/
-	public static function testPrivilegeBefore() {
-		assertEquals(User::getPrivileges(User::getID('Bob')), User::PRIVILEGE_NONE, 'User "Bob" should have zero privileges in the beginning.');
+	public function testPrivilegeBefore() {
+		$this->assertEquals(User::getPrivileges(User::getID('Bob')), User::PRIVILEGE_NONE, 'User "Bob" should have zero privileges in the beginning.');
 	}
 
 	/**
 	* @depends testCreate
 	*/
-	public static function testDelete() {
+	public function testDelete() {
 		# not implemented yet
 	}
 
@@ -73,27 +72,27 @@ class UserTest extends PHPUnit_Framework_TestCase {
 		$db_connection->query($db_query);
 	}
 
-	public static function testPrivilegeArray() {
-		assertEquals(User::privilegeToArray(User::PRIVILEGE_NONE), array('none'), 'Failed to convert privilege PRIVILEGE_NONE to array');
-		assertEquals(User::privilegeToArray(User::PRIVILEGE_NONE|User::PRIVILEGE_REVIEW), array('review'), 'Failed to convert privilege PRIVILEGE_NONE|PRIVILEGE_REVIEW to array');
+	public function testPrivilegeArray() {
+		$this->assertEquals(User::privilegeToArray(User::PRIVILEGE_NONE), array('none'), 'Failed to convert privilege PRIVILEGE_NONE to array');
+		$this->assertEquals(User::privilegeToArray(User::PRIVILEGE_NONE|User::PRIVILEGE_REVIEW), array('review'), 'Failed to convert privilege PRIVILEGE_NONE|PRIVILEGE_REVIEW to array');
 
 		$arr = User::privilegeToArray(User::PRIVILEGE_ADMIN|User::PRIVILEGE_STDLIB_ADMIN|User::PRIVILEGE_REGISTRATION);
-		assertInternalType('array', $arr, 'Privilege conversion did not return an array');
-		assertCount(3, $arr, 'Invalid element count in privilege array');
-		assertContains('admin', $arr, 'Failed to convert privilege PRIVILEGE_ADMIN|PRIVILEGE_STDLIB_ADMIN|PRIVILEGE_REGISTRATION to array');
-		assertContains('stdlib-admin', $arr, 'Failed to convert privilege PRIVILEGE_ADMIN|PRIVILEGE_STDLIB_ADMIN|PRIVILEGE_REGISTRATION to array');
-		assertContains('registration', $arr, 'Failed to convert privilege PRIVILEGE_ADMIN|PRIVILEGE_STDLIB_ADMIN|PRIVILEGE_REGISTRATION to array');
+		$this->assertInternalType('array', $arr, 'Privilege conversion did not return an array');
+		$this->assertCount(3, $arr, 'Invalid element count in privilege array');
+		$this->assertContains('admin', $arr, 'Failed to convert privilege PRIVILEGE_ADMIN|PRIVILEGE_STDLIB_ADMIN|PRIVILEGE_REGISTRATION to array');
+		$this->assertContains('stdlib-admin', $arr, 'Failed to convert privilege PRIVILEGE_ADMIN|PRIVILEGE_STDLIB_ADMIN|PRIVILEGE_REGISTRATION to array');
+		$this->assertContains('registration', $arr, 'Failed to convert privilege PRIVILEGE_ADMIN|PRIVILEGE_STDLIB_ADMIN|PRIVILEGE_REGISTRATION to array');
 	}
 
-	public static function testArrayPrivilege() {
+	public function testArrayPrivilege() {
 		$arr = array('stdlib', 'review');
-		assertEquals(User::privilegeFromArray($arr), User::PRIVILEGE_STDLIB|User::PRIVILEGE_REVIEW, 'Failed to convert array [stdlib, review] to privilege');
+		$this->assertEquals(User::privilegeFromArray($arr), User::PRIVILEGE_STDLIB|User::PRIVILEGE_REVIEW, 'Failed to convert array [stdlib, review] to privilege');
 
 		$arr = array('admin');
-		assertEquals(User::privilegeFromArray($arr), User::PRIVILEGE_ADMIN, 'Failed to convert array [admin] to privilege');
+		$this->assertEquals(User::privilegeFromArray($arr), User::PRIVILEGE_ADMIN, 'Failed to convert array [admin] to privilege');
 
 		$arr = array();
-		assertEquals(User::privilegeFromArray($arr), User::PRIVILEGE_NONE, 'Failed to convert array [] to privilege');
+		$this->assertEquals(User::privilegeFromArray($arr), User::PRIVILEGE_NONE, 'Failed to convert array [] to privilege');
 	}
 }
 ?>
