bccc0326355bd5d00f665ea070bf11defe85896e
maul.esel <maul.esel@go4more.de>
2013-05-22 01:09:05 +0200

    just visual: indent output of installation script

--- tests/setup/php-pear.sh
+++ tests/setup/php-pear.sh
@@ -1,11 +1,11 @@
 
 echo "Installing PHP packages..."
 if [ "$TRAVIS_PHP_VERSION" = "5.5" ]; then
-	echo "	Using pyrus..."
-	pyrus channel-discover pear.phpunit.de
-	pyrus install phpunit/DbUnit
+	echo "    Using pyrus..."
+	pyrus channel-discover pear.phpunit.de | sed 's/^/        /'
+	pyrus install phpunit/DbUnit | sed 's/^/        /'
 else
-	echo "	Using pear..."
-	pear channel-discover pear.phpunit.de
-	pear install phpunit/DbUnit
+	echo "    Using pear..."
+	pear channel-discover pear.phpunit.de | sed 's/^/        /'
+	pear install phpunit/DbUnit | sed 's/^/        /'
 fi
