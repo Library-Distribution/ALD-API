dc8ccbd6ddefe324c0b30cd76c3c5367b26a6ece
maul.esel <maul.esel@go4more.de>
2013-06-15 17:39:04 +0200

    fix SuspensionTest class and make it a DB test

--- tests/users/SuspensionTest.php
+++ tests/users/SuspensionTest.php
@@ -1,100 +1,96 @@
 <?php
-require_once 'PHPUnit/Framework/TestCase.php';
+require_once dirname(__FILE__) . '/../ALD_Database_TestCase.php';
 
+require_once dirname(__FILE__) . '/../../config/database.php';
 require_once dirname(__FILE__) . '/../../User.php';
-require_once dirname(__FILE__) . '/../../db.php';
 require_once dirname(__FILE__) . '/../../users/Suspension.php';
 
-class SuspensionTest extends PHPUnit_Framework_TestCase {
+class SuspensionTest extends ALD_Database_TestCase {
 
-	private static $id;
-	private static $s1;
-	private static $s2;
+	const USER1_NAME = 'Frank';
+	const USER1_ID = '16A143CE32B14162ABA8C08EA962B42A';
 
-	const USER_NAME = 'Frank';
-	const EXPIRATION_DATE = '2037-04-15 00:03:49';
+	const USER2_NAME = 'Ben';
+	const USER2_ID = '281B3C505B5A4CE59DA856E39C9470F5';
 
-	public static function setUpBeforeClass() {
-		User::create(self::USER_NAME, 'frank@example.com', '1234');
-		self::$id = User::getID(self::USER_NAME);
+	public function test_isSuspended() {
+		$this->assertFalse(Suspension::isSuspended(self::USER1_NAME));
+		$this->assertTrue(Suspension::isSuspended(self::USER2_NAME));
 	}
 
-	public static function tearDownAfterClass() {
-		$db_connection = db_ensure_connection();
-
-		$db_query = 'DELETE FROM ' . DB_TABLE_USERS . ' WHERE `name` = "' . self::USER_NAME . '"';
-		$db_connection->query($db_query);
-
-		$db_query = 'DELETE FROM ' . DB_TABLE_SUSPENSIONS . ' WHERE `user` = UNHEX("' . self::$id . '")';
-		$db_connection->query($db_query);
-	}
-
-	public function testBeforeSuspension() {
-		$this->assertFalse(Suspension::isSuspended(self::USER_NAME));
-		$this->assertFalse(Suspension::isSuspendedById(self::$id));
+	public function test_isSuspendedById() {
+		$this->assertFalse(Suspension::isSuspendedById(self::USER1_ID));
+		$this->assertTrue(Suspension::isSuspendedById(self::USER2_ID));
 	}
 
 	/**
-	* @depends testBeforeSuspension
+	* @depends test_isSuspended
 	*/
-	public function testCreate() {
-		self::$s1 = Suspension::create(self::USER_NAME, 'Suspended for no particular reason');
-		$this->assertInternalType('int', self::$s1, 'Suspension creation did not return an integer');
-		$this->assertTrue(Suspension::isSuspended(self::USER_NAME));
+	public function test_create() {
+		$count_before = $this->getConnection()->getRowCount(DB_TABLE_SUSPENSIONS);
+
+		$s = Suspension::create(self::USER1_NAME, 'Suspended for no particular reason');
+		$this->assertInternalType('int', $s, 'Suspension creation did not return an integer');
+
+		$this->assertEquals($count_before + 1, $this->getConnection()->getRowCount(DB_TABLE_SUSPENSIONS));
+		$this->assertTrue(Suspension::isSuspended(self::USER1_NAME));
 	}
 
 	/**
-	* @depends testBeforeSuspension
+	* @depends test_isSuspendedById
 	*/
-	public function testCreateForId() {
-		self::$s2 = Suspension::createForId(self::$id, 'For testing reasons', self::EXPIRATION_DATE);
-		$this->assertInternalType('int', self::$s2, 'Suspension creation (for ID) did not return an integer');
-		$this->assertTrue(Suspension::isSuspendedById(self::$id));
+	public function test_createForId() {
+		$count_before = $this->getConnection()->getRowCount(DB_TABLE_SUSPENSIONS);
+
+		$s = Suspension::createForId(self::USER1_ID, 'For testing reasons', '2037-04-15 00:03:49');
+		$this->assertInternalType('int', $s, 'Suspension creation (for ID) did not return an integer');
+
+		$this->assertEquals($count_before + 1, $this->getConnection()->getRowCount(DB_TABLE_SUSPENSIONS));
+		$this->assertTrue(Suspension::isSuspendedById(self::USER1_ID));
 	}
 
-	/**
-	* @depends testCreate
-	* @depends testCreateForId
-	*/
-	public function testRetrieveList() {
-		$s = Suspension::getSuspensions(self::USER_NAME);
+	public function test_getSuspensions() {
+		$s = Suspension::getSuspensions(self::USER2_NAME);
 
 		$this->assertInternalType('array', $s, 'Suspension retrieval did not return an array');
 		$this->assertCount(2, $s, 'Incorrect number of suspensions: ' . count($s));
+	}
 
-		$this->assertEquals($s[0], Suspension::getSuspension($s[0]->id), 'Should equal individually retrieved suspension');
+	public function test_getSuspensionsById() {
+		$s = Suspension::getSuspensionsById(self::USER2_ID);
+
+		$this->assertInternalType('array', $s, 'Suspension retrieval did not return an array');
+		$this->assertCount(2, $s, 'Incorrect number of suspensions: ' . count($s));
 	}
 
 	/**
-	* @depends testCreate
+	* @depends test_getSuspensionsById
+	* @depends test_isSuspendedById
 	*/
-	public function testFirstSuspension() {
-		$s = Suspension::getSuspension(self::$s1);
-
-		$this->assertTrue($s->restricted, 'Suspension should be restricted but is not.');
-		$this->assertTrue($s->infinite, 'Suspension should be infinite but is not.');
-		$this->assertEquals($s->expires, NULL, 'Expiration date should be NULL');
-	}
-
-	/**
-	* @depends testCreateForId
-	*/
-	public function testSecondSuspension() {
-		$s = Suspension::getSuspension(self::$s2);
-
-		$this->assertTrue($s->restricted, 'Suspension should be restricted but is not.');
-		$this->assertFalse($s->infinite, 'Suspension should be infinite but is not.');
-		$this->assertEquals($s->expires, new DateTime(self::EXPIRATION_DATE), 'Expiration date not set properly');
-	}
-
-	/**
-	* @depends testRetrieveList
-	*/
-	public function testDelete() {
-		foreach (Suspension::getSuspensionsById(self::$id) AS $s) {
+	public function test_delete() {
+		foreach (Suspension::getSuspensionsById(self::USER2_ID) AS $s) {
 			$s->delete();
 		}
-		$this->assertFalse(Suspension::isSuspendedById(self::$id));
+
+		foreach (Suspension::getSuspensionsById(self::USER2_ID) AS $s) {
+			$this->assertFalse($s->active);
+		}
+		$this->assertFalse(Suspension::isSuspendedById(self::USER2_ID));
+	}
+
+	/**
+	* @depends test_getSuspensionsById
+	*/
+	public function test__suspensions_valid() {
+		$s = Suspension::getSuspensionsById(self::USER2_ID);
+
+		$this->assertTrue($s[0]->restricted, 'Suspension should be restricted but is not.');
+		$this->assertTrue($s[0]->infinite, 'Suspension should be infinite but is not.');
+		$this->assertEquals($s[0]->expires, NULL, 'Expiration date should be NULL');
+
+		$this->assertFalse($s[1]->restricted, 'Suspension should not be restricted but is.');
+		$this->assertFalse($s[1]->infinite, 'Suspension should not be infinite but is.');
+		$this->assertEquals($s[1]->expires, new DateTime('2033-08-05 12:00:05'), 'Expiration date not set properly');
 	}
 }
 ?>
