eae5c42f3aca47d8a561c4f52389aa45a5a8db70
maul.esel <maul.esel@go4more.de>
2013-07-01 01:33:20 +0200

    substitute env var

--- tests/setup/apache.sh
+++ tests/setup/apache.sh
@@ -2,7 +2,11 @@
 
 sudo a2enmod rewrite
 sudo cp tests/setup/vhost.conf /etc/apache2/sites-enabled/default
+
+TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR//\//\\/}" # escape for below
+sudo sed -i -e "s/TRAVIS_BUILD_DIR/'$TRAVIS_BUILD_DIR'/g" /etc/apache2/sites-enabled/default
+
 sudo service apache2 restart
 
 # debug:
-curl -i http://localhost/.travis.yml
+curl -i http://localhost/items/list
