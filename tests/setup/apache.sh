5791e422b6d1c5b05eac415424cc52277c562709
maul.esel <maul.esel@go4more.de>
2013-07-01 02:37:08 +0200

    remove debug code

--- tests/setup/apache.sh
+++ tests/setup/apache.sh
@@ -7,7 +7,3 @@ TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR//\//\\/}" # escape for below
 sudo sed -i -e "s/TRAVIS_BUILD_DIR/'$TRAVIS_BUILD_DIR'/g" /etc/apache2/sites-enabled/default
 
 sudo service apache2 restart
-
-# debug:
-apache2ctl -S
-curl -i -f http://localhost/items/list
