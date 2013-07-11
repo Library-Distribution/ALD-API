4ce9b58f2495a1b4dfd549b2cfe502dbe641f01f
maul.esel <maul.esel@go4more.de>
2013-07-11 22:16:00 +0200

    ContentNegotiator: save converter instances

--- ContentNegotiator.php
+++ ContentNegotiator.php
@@ -7,28 +7,35 @@ require_once(dirname(__FILE__) . '/YamlConverter.php');
 
 class ContentNegotiator {
 	private static $output_handlers = array('application/json' => 'JsonConverter', 'text/x-yaml' => 'YamlConverter');
+	private static $converter_instances = array();
 
 	public static function SupportedMimeTypes() {
 		$mimes = array();
-		$instances = array();
 
 		foreach (self::$output_handlers AS $mime => $handler) {
-			if (!isset($instances[$handler])) {
-				if (!class_exists($handler)) {
-					continue;
-				}
-				$instances[$handler] = new $handler();
-				if (!($instances[$handler] instanceof OutputConverter)) {
-					throw new HttpException(500, NULL, 'Unsupported converter for mime type "' . $mime . '"!');
-				}
-			}
-
-			if ($instances[$handler]->canRun()) {
+			$converter = self::get_converter($mime);
+			if ($converter->canRun()) {
 				$mimes[] = $mime;
 			}
 		}
 
 		return $mimes;
+	}
+
+	private static get_converter($mime) {
+		$class = self::$output_handlers[$mime];
+
+		if (!isset(self::$converter_instances[$class])) {
+			if (!class_exists($class)) {
+				continue;
+			}
+			self::$converter_instances[$class] = new $class();
+			if (!(self::$converter_instances[$class] instanceof OutputConverter)) {
+				throw new HttpException(500, NULL, 'Unsupported converter for mime type "' . $mime . '"!');
+			}
+		}
+
+		return self::$converter_instances[$class];
 	}
 
 	public static function MimeType() {
@@ -49,8 +56,8 @@ class ContentNegotiator {
 
 	public static function Output(array $data, $namespace) {
 		$mime = self::MimeType();
-		$conv = new self::$output_handlers[$mime]();
-		$content = $conv->convert($data, $namespace); # canRun() is checked by <SupportedMimeTypes> already
+		$converter = self::get_converter($mime);
+		$content = $converter->convert($data, $namespace); # canRun() is checked by <SupportedMimeTypes> already
 
 		header('Content-Type: ' . $mime);
 		echo $content;
